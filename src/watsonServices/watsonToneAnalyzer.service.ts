b004a699b5fbf20ee40135e40f6947af5fb56b17
saifrehman <saif.rehman1@ibm.com>
2018-02-19 22:19:40 +0400

    completed service for watson tone analyzer

---
+++ src/watsonServices/watsonToneAnalyzer.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WatsonConfig } from './watson.config';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class watsonToneAnalyzer {
+    constructor(
+        private http: Http
+    ){ }
+    
+    public analyzeToneOnGivenText(text: string): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.toneAnalyzer.authUsername+":"+WatsonConfig.authURL.toneAnalyzer.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`
+            })
+        });
+        const link = WatsonConfig.authURL.toneAnalyzer.baseLink;
+        const bodyObject = {
+            tone_input: text,
+            version: WatsonConfig.authURL.toneAnalyzer.version_date
+        };
+        const bodyString = JSON.stringify(bodyObject); // Stringify payload
+        return this.http.post(link, bodyObject, options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
+}
