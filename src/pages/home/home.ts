9d9771177252bc1d2540008980df96c383801097
saifrehman <saif.rehman1@ibm.com>
2018-02-28 16:45:05 +0400

    changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,28 +7,17 @@ import { watsonTranslation } from '../../watsonServices/watsonTranslation.servic
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public show:any="nothing"
-  public randomImage:string = "https://loremflickr.com/200/200"
+  public show: any = "nothing"
+  public randomImage: string = "https://loremflickr.com/200/200"
   public url: string = "http://lorempixel.com/100/100/";
-  constructor(public navCtrl: NavController, public WatsonVisualRecognition:watsonVisualRecognition, public WatsonTranslation:watsonTranslation) {
+  constructor(public navCtrl: NavController, public WatsonVisualRecognition: watsonVisualRecognition, public WatsonTranslation: watsonTranslation) {
+    this.WatsonVisualRecognition.getVisualRecognitonDataByOnlyURL(this.randomImage).subscribe((data) => {
+      console.log(data);
+    },
+      (error) => {
+        console.log(error)
+      });
   }
 
-  doRefresh(refresher) {
-    this.randomImage = "https://loremflickr.com/200/200"
-    this.WatsonVisualRecognition.getVisualRecognitonDataByOnlyURL(this.randomImage).subscribe((data) => {
-      console.log('data', data);
-      refresher.complete();
-      this.show = data['images'][0]['classifiers'][0]['classes'][0]['class']
-      this.WatsonTranslation.translateToALanguage(this.show, 'en-ar').subscribe((data) => {
-       this.show = data['translations'][0]['translation']
-    },
-    (error) => {
-        console.log(error)
-    })
-  },
-  (error) => {
-      console.log(error)
-      refresher.complete();
-  });
-  }
+
 }
