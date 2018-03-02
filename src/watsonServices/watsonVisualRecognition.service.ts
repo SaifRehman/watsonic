2a97cbc3869b4bfac2f2a46e84561518389efa2f
saifrehman <saif.rehman1@ibm.com>
2018-03-02 20:59:42 +0400

    update readme

--- src/watsonServices/watsonVisualRecognition.service.ts
+++ src/watsonServices/watsonVisualRecognition.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import * as fs from 'fs';
+import * as fs from 'browserify-fs';
+// import { createReadStream } from 'fs';
+
+
 import { WatsonConfig } from './watson.config';
 import { Headers, Http, RequestOptions, Response } from '@angular/http';
 import { error } from 'util';
@@ -9,12 +12,10 @@ export class watsonVisualRecognition {
     public parameters: any;
     public params: any;
     public temp:any;
-
     public constructor(public http: Http) {
         this.parameters = {};
         this.params = {};
     }
-
     public getVisualRecognitonDataByOnlyURL(url): Observable<any> {
         const link = WatsonConfig.authURL.wantsonVisualRecognition.baseLinkClassify +
             WatsonConfig.authURL.wantsonVisualRecognition.api_key.toString() + "&url=" +
@@ -33,7 +34,6 @@ export class watsonVisualRecognition {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
-
     public getVisualRecognitonDataByOnlyURLWithHighConfidenceOnly(url): Observable<any> {
         const link = WatsonConfig.authURL.wantsonVisualRecognition.baseLinkClassify +
             WatsonConfig.authURL.wantsonVisualRecognition.api_key.toString() + "&url=" +
@@ -59,7 +59,6 @@ export class watsonVisualRecognition {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
-
     public getVisualRecognitonForFacesDataByOnlyURL(url): Observable<any> {
         const link = WatsonConfig.authURL.wantsonVisualRecognition.baseLinkClassifyFace +
             WatsonConfig.authURL.wantsonVisualRecognition.api_key.toString() + "&url=" +
@@ -76,5 +75,36 @@ export class watsonVisualRecognition {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
+
+    public createClassifiersFromGivenFiles(): Observable<any> {
+        const link = WatsonConfig.authURL.wantsonVisualRecognition.baseLinkClassifier +
+            WatsonConfig.authURL.wantsonVisualRecognition.api_key.toString() + 
+            "&version=" + WatsonConfig.authURL.wantsonVisualRecognition.version_date.toString();
+                    fs.readFile('./cats.zip', function(err, data) {
+                        console.log(data);
+                    });
+            const bodyObject = {
+            // name:'dogs', 
+            // beagle_positive_examples: fs.,
+            // goldenretriever_positive_examples: createReadStream('golden-retriever.zip'),
+            // husky_positive_examples: createReadStream('husky.zip'),
+            // negative_examples: createReadStream('cats.zip')
+        };
+        const bodyString = JSON.stringify(bodyObject); // Stringify payload
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        });
+        return this.http.post(link, bodyObject, options) // ...using post request
+            .map((res) => {
+                return JSON.parse(res['_body']);
+            })
+            .catch((error: any) => {
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
 }
+
+// creating custom classifers
 
