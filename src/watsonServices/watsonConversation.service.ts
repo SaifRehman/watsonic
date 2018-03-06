73a9f0bd1c9f690f5c1a21eb3a794e926e03f1bc
saifrehman <saif.rehman1@ibm.com>
2018-03-06 17:59:14 +0400

    #23 list entities service created

--- src/watsonServices/watsonConversation.service.ts
+++ src/watsonServices/watsonConversation.service.ts
@@ -239,4 +239,22 @@ export class watsonConversation {
             });
     }
 
+    public listEntities (workspaceid): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.conversation.authUsername+":"+WatsonConfig.authURL.conversation.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`
+            })
+        });
+        const link = WatsonConfig.authURL.conversation.baseLinkWorkspace + '/' + workspaceid + '/' + 'entities' +
+        '?version=' + WatsonConfig.authURL.conversation.version_date;
+        return this.http.get(link, options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
+
 }
