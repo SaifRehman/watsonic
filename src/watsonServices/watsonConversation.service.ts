585494aef822e78e63d213b8ac967ef9220f0978
saifrehman <saif.rehman1@ibm.com>
2018-03-06 21:02:49 +0400

    #23 update synonym service created

--- src/watsonServices/watsonConversation.service.ts
+++ src/watsonServices/watsonConversation.service.ts
@@ -522,4 +522,28 @@ export class watsonConversation {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
+
+    public updateSynonym(workspaceid,entity,value, oldsynonym,synonym): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.conversation.authUsername+":"+WatsonConfig.authURL.conversation.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`
+            })
+        });
+        const link = WatsonConfig.authURL.conversation.baseLinkWorkspace + '/' + workspaceid + '/' + 'entities' +
+        + '/' + entity + '/values/' + value + '/' + 'synonyms' + '/' + oldsynonym +
+        '?version=' + WatsonConfig.authURL.conversation.version_date;
+        const bodyObject = {
+            synonym,
+        };
+        const bodyString = JSON.stringify(bodyObject); // Stringify payload
+        return this.http.post(link,bodyString,options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
+    
 }
