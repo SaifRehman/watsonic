baf0c2304d87e1d58f0b537e6bca83c1f50e3b60
saifrehman <saif.rehman1@ibm.com>
2018-03-06 15:53:30 +0400

    in progress

---
+++ src/watsonServices/watsonConversation.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WatsonConfig } from './watson.config';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
+import { error } from 'util';
+@Injectable()
+export class watsonConversation {
+ 
+    public constructor(public http: Http) {
+
+    }
+}
