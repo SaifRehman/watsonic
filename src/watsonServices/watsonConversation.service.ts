b4972266feb977e53e3a207d08e3de14193e38f4
saifrehman <saif.rehman1@ibm.com>
2018-03-06 23:09:12 +0400

    #23 delete examples service created

--- src/watsonServices/watsonConversation.service.ts
+++ src/watsonServices/watsonConversation.service.ts
@@ -827,4 +827,23 @@ export class watsonConversation {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
+
+    public deleteExamples(workspaceid,intent,text): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.conversation.authUsername+":"+WatsonConfig.authURL.conversation.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`,
+            })
+        });
+        const link = WatsonConfig.authURL.conversation.baseLinkWorkspace + '/' + workspaceid + '/' + 'intent' +
+        '/' + 'examples'+ '/' + text + 
+        '?version=' + WatsonConfig.authURL.conversation.version_date;
+        return this.http.delete(link,options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
 }
