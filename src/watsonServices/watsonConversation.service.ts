9292ecf9b28c9ffa01b22992e030f0ddba681cb3
saifrehman <saif.rehman1@ibm.com>
2018-03-06 16:59:36 +0400

    #23 create workspace service created

--- src/watsonServices/watsonConversation.service.ts
+++ src/watsonServices/watsonConversation.service.ts
@@ -26,4 +26,30 @@ export class watsonConversation {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
+
+    public createWorkspace(name,intents,entities,language,description,dialog_nodes): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.conversation.authUsername+":"+WatsonConfig.authURL.conversation.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`
+            })
+        });
+        const link = WatsonConfig.authURL.conversation.baseLinkWorkspace + WatsonConfig.authURL.conversation.version_date;
+        const bodyObject = {
+            name,
+            intents,
+            entities,
+            language,
+            description,
+            dialog_nodes,
+        };
+        const bodyString = JSON.stringify(bodyObject); // Stringify payload
+        return this.http.post(link,bodyString,options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
 }
