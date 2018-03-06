1ea445ae4ce92265124e177f101f19afc6eef966
saifrehman <saif.rehman1@ibm.com>
2018-03-06 22:24:57 +0400

    #23 update dialog node service created

--- src/watsonServices/watsonConversation.service.ts
+++ src/watsonServices/watsonConversation.service.ts
@@ -728,4 +728,26 @@ export class watsonConversation {
                 return Observable.throw(error.json().error || 'Server error');
             });
     }
+
+    public updateDialogNode(workspaceid,dialog_node,conditions,output,title): Observable<any> {
+        const token = btoa(WatsonConfig.authURL.conversation.authUsername+":"+WatsonConfig.authURL.conversation.authPassword);
+        const options = new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${token}`
+            })
+        });
+        const link = WatsonConfig.authURL.conversation.baseLinkWorkspace + '/' + workspaceid + '/' + 'dialog_nodes' + '/' +
+        dialog_node + '?version=' + WatsonConfig.authURL.conversation.version_date;
+        const bodyObject = {
+            output
+        };
+        const bodyString = JSON.stringify(bodyObject); // Stringify payload
+        return this.http.post(link,bodyString,options) // ...using post request
+            .map((res: Response) => res.json())
+            .catch((error: any) => {
+                console.log(error);
+                return Observable.throw(error.json().error || 'Server error');
+            });
+    }
 }
